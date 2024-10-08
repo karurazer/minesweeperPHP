c0f10cddd1503f1bb20ae27200df1df537917010
karurazer <karurazer@gmail.com>
2024-10-08 15:05:27 +0200

    Start point

--- index.php
+++ index.php
@@ -7,25 +7,50 @@
 </head>
 <body>
     <?php
-    class Board{
-        public $size = ['x'=>10, 'y'=>10];
-    
-        public $number_of_bombs = 10;
-        private $board=[];
-    
-        function BoardGenerator(){
+    class Cell{
+        private $is_mine = false;
+        private $is_open = false;
+        private $mines_around = 0;
+    }
+    class Minesweeper{
+        public $columns;
+        public $rows;
+        private $size;
+        public $bombs;
+        private $board = [];
+        function __construct($columns = 8, $rows = 8, $bombs=10){
+            $this->size = $this->columns * $this->rows;
+            $this->columns = $columns;
+            $this->rows = $rows;
+            $this->size = $this->rows * $this->columns;
+            $this->bombs = $bombs;
             
-            for ($j=0;$j<$this->size['y'];$j++){
+            for ($j=0;$j<$this->rows;$j++){
                 $line=[];
-                for ($i=0; $i < $this->size['x']; $i++) { 
-                    $line[] = 0;
+                for ($i=0; $i < $this->columns; $i++) { 
+                    $line[] = new Cell();
                 }
                 $this->board[] = $line;
             }
+            
+
         }
-    
+        
+        function place_mines(){
+            
+        }
+        function find_neighbors(){
+
+        }
+        function play(){
+            
+        }
+        function show_board(){
+            var_dump($this->board);
+        }
     }
-$a = new Board();
+$a = new Minesweeper();
+$a->show_board()
     ?>
 </body>
 </html>
